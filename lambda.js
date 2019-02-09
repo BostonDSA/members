68fdb00baf39fa27105b68032597085754c1a48f
Alexander Mancevice <amancevice@cargometrics.com>
2019-02-09 10:53:08 -0500

    Clean up JS

--- lambda.js
+++ lambda.js
@@ -1,33 +1,23 @@
 'use strict';
+const AWS                  = require('aws-sdk');
 const awsServerlessExpress = require('aws-serverless-express');
-const secret = process.env.AWS_SECRET;
+
+const AWS_SECRET = process.env.AWS_SECRET;
+
+const secretsmanager = new AWS.SecretsManager();
 
 let server;
 
-function fetchEnv (name) {
-  console.log(`GET ${name}`);
-  const AWS = require('aws-sdk');
-  const secretsmanager = new AWS.SecretsManager();
-  return secretsmanager.getSecretValue({SecretId: name}).promise().then((data) => {
-    const env = JSON.parse(data.SecretString);
-    Object.keys(env).map((key) => { process.env[key] = env[key]; });
-    return env;
-  });
-}
-
-function getServer () {
-  if (server) {
-    return Promise.resolve(server);
-  } else {
-    return fetchEnv(secret).then((env) => {
-      const app = require('./app');
-      server = awsServerlessExpress.createServer(app);
-      return server;
-    });
-  }
-}
+const getServer = async (options) => {
+  const secret = await secretsmanager.getSecretValue(options).promise();
+  Object.assign(process.env, JSON.parse(secret.SecretString));
+  server = awsServerlessExpress.createServer(require('./app'));
+  return server;
+};
 
 exports.handler = (event, context) => {
   console.log(`EVENT ${JSON.stringify(event)}`);
-  getServer().then((server) => awsServerlessExpress.proxy(server, event, context));
+  Promise.resolve(server || getServer({SecretId: AWS_SECRET})).then((server) => {
+    awsServerlessExpress.proxy(server, event, context);
+  });
 };
