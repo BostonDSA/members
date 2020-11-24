8100ae93b2b4e72dee85b89bcb7721db71383425
Josh King <skierx@gmail.com>
2020-11-23 23:37:34 -0500

    * Applying some ESLint cleanup * Breaking out functions

--- zoom_meeting_fetcher.js
+++ zoom_meeting_fetcher.js
@@ -1,6 +1,6 @@
 const AWS = require('aws-sdk');
 const axios = require('axios');
-const { DateTime } = require("luxon");
+const {DateTime} = require('luxon');
 const jwt = require('jsonwebtoken');
 
 const AWS_SECRET = process.env.AWS_SECRET;
@@ -10,110 +10,131 @@ const s3 = new AWS.S3();
 const secretsmanager = new AWS.SecretsManager();
 
 const accounts = {
-    "1": "info@bostondsa.org",
-    "2": "treasurer@bostondsa.org",
-    "3": "zoom@bostondsa.org"
-}
-
-// Used for collecting the raw meetings
-const meetings_from_zoom = [];
-
-// Storing formatted meetings
-const meetings_by_date = {
-    'meetings': {},
-    'dates': []
+  '1': 'info@bostondsa.org',
+  '2': 'treasurer@bostondsa.org',
+  '3': 'zoom@bostondsa.org',
 };
 
-// Zoom secrets stored in secrets manager
-// These are generated via https://marketplace.zoom.us: Develop > Build JWT App
-const load_secrets = async (options) => {
-    const secret = await secretsmanager.getSecretValue(options).promise();
-    Object.assign(process.env, JSON.parse(secret.SecretString));
-  };
+// Used for collecting the raw meetings
+const meetingsFromZoom = [];
+
+// Storing formatted meetings
+const meetingsByDate = {
+  'meetings': {},
+  'dates': [],
+};
+
+let token;
+
+/**
+ * Load Zoom secrets stored in AWS Secrets Manager.
+ * These are generated via https://marketplace.zoom.us: Develop > Build JWT App
+ *
+ * @param {*} options
+ */
+async function loadSecrets(options) {
+  const secret = await secretsmanager.getSecretValue(options).promise();
+  Object.assign(process.env, JSON.parse(secret.SecretString));
+};
+
+/**
+ * Filtering for dates that are close to current
+ *
+ * @param {*} meeting
+ * @return {boolean} true if the meeting is close to today
+ */
+function isMeetingSoon(meeting) {
+  const local = DateTime.local().setZone(meeting['timezone']);
+  return (
+    meeting > local.minus({days: 1}) ||
+    meeting < local.plus({days: 21})
+  );
+}
 
 exports.handler = (event, context) => {
+  loadSecrets({SecretId: AWS_SECRET}).then(() => {
+    const payload = {
+      iss: process.env.ZOOM_API_KEY,
+      exp: ((new Date()).getTime() + 5000),
+    };
+    token = jwt.sign(payload, process.env.ZOOM_API_SECRET);
 
-    load_secrets({SecretId: AWS_SECRET}).then(() => {
+    const zoomApiCalls = [];
 
-        const payload = {
-            iss: process.env.ZOOM_API_KEY,
-            exp: ((new Date()).getTime() + 5000)
-        };
-        const token = jwt.sign(payload, process.env.ZOOM_API_SECRET);
+    for (const prop in accounts) {
+      const email = accounts[prop];
+      const prom = axios.get(
+          'https://api.zoom.us/v2/users/' + email + '/meetings?page_size=300',
+          {headers: {'Authorization': `Bearer ${token}`}})
+          .then(function(response) {
+            console.log(response.data.meetings);
 
-        let zoom_api_calls = [];
+            response.data.meetings.forEach((meeting) => {
+              const meetingStart = DateTime.fromISO(meeting['start_time'])
+                  .setZone(meeting['timezone']);
 
-        for (const prop in accounts) {
-            const email = accounts[prop];
-            let prom = axios.get("https://api.zoom.us/v2/users/" + email + "/meetings?page_size=300", {headers: {"Authorization" : `Bearer ${token}`}})
-                .then(function(response) {
+              if (!isMeetingSoon(meetingStart)) {
+                return;
+              }
 
-                    response.data.meetings.forEach(element => {
+              // Decorate additional properties for display and sorting
 
-                        const meeting_start_dt = DateTime.fromISO(element['start_time']).setZone(element['timezone']);
-                        const local = DateTime.local().setZone(element['timezone']);
+              const meetingEnd = meetingStart.plus({minutes: meeting['duration']});
+              meeting['time_range'] = meetingStart.toLocaleString(DateTime.TIME_SIMPLE) +
+                '\u2013' +
+                meetingEnd.toLocaleString(DateTime.TIME_SIMPLE);
+              meeting['med_date'] = meetingStart.toLocaleString(DateTime.DATE_MED);
+              meeting['zoom_account'] = prop;
+              meeting['short_date'] = meetingStart.toFormat('yyyyMMdd');
+              meeting['millis'] = meetingStart.toMillis();
 
-                        if (meeting_start_dt < local.minus({days: 1}) ||
-                        meeting_start_dt > local.plus({days: 20})
-                        ) {
-                            return;
-                        }
+              meetingsFromZoom.push(meeting);
+            });
 
-                        // Decorate additional properties for display and sorting
+            console.log('Fetched meetings for zoom account: ' + prop);
+          })
+          .catch(function(error) {
+            console.error(error);
+          });
+      zoomApiCalls.push(prom);
+    }
 
-                        const meeting_end_dt = meeting_start_dt.plus({ minutes: element['duration'] });
-                        element['time_range'] = meeting_start_dt.toLocaleString(DateTime.TIME_SIMPLE) + "\u2013" +
-                            meeting_end_dt.toLocaleString(DateTime.TIME_SIMPLE);
-                        element['med_date'] = meeting_start_dt.toLocaleString(DateTime.DATE_MED);
-                        element['zoom_account'] = prop;
-                        element['short_date'] = meeting_start_dt.toFormat("yyyyMMdd");
-                        element['millis'] = meeting_start_dt.toMillis();
+    Promise.all(zoomApiCalls).then(function(results) {
+      console.log('All meetings fetched, assembling data.');
 
-                        meetings_from_zoom.push(element)
-                    });
+      // sort first, then we can just assemble the final data in order
 
-                    console.log('Fetched meetings for zoom account: ' + prop);
-                })
-                .catch(function(error) {
-                    console.error(error)
-                });
-            zoom_api_calls.push(prom);
+      meetingsFromZoom.sort(function(a, b) {
+        return a['millis'] - b['millis'];
+      });
+
+      meetingsFromZoom.forEach((meeting) => {
+        const shortDate = meeting['short_date'];
+        if (!(shortDate in meetingsByDate['meetings'])) {
+          meetingsByDate['meetings'][shortDate] = [];
         }
 
-        Promise.all(zoom_api_calls).then(function (results) {
-            console.log('All meetings fetched, assembling data.');
+        meetingsByDate['meetings'][shortDate].push(meeting);
+        meetingsByDate['dates'].push(shortDate);
+      });
 
-            // sort first, then we can just assemble the final data in order
+      meetingsByDate['dates'] = Array.from(new Set(meetingsByDate['dates']));
+      meetingsByDate['dates'].sort();
 
-            meetings_from_zoom.sort(function(a, b) {
-                return a['millis'] - b['millis'];
-            });
+      const params = {
+        Bucket: AWS_BUCKET,
+        Key: 'zoom_meetings.json', // File name you want to save as in S3
+        Body: JSON.stringify(meetingsByDate),
+      };
 
-            meetings_from_zoom.forEach(element => {
-                let short_date = element['short_date'];
-                if (!(short_date in meetings_by_date['meetings'])) {
-                    meetings_by_date['meetings'][short_date] = [];
-                }
+      s3.upload(params, function(err, data) {
+          if (err) {
+              throw err;
+          }
+          console.log('zoom_meetings.json uploaded to S3');
+      });
 
-                meetings_by_date['meetings'][short_date].push(element);
-                meetings_by_date['dates'].push(short_date);
-            });
-
-            meetings_by_date['dates'] = Array.from(new Set(meetings_by_date['dates']));
-            meetings_by_date['dates'].sort();
-
-            const params = {
-                Bucket: AWS_BUCKET,
-                Key: 'zoom_meetings.json', // File name you want to save as in S3
-                Body: JSON.stringify(meetings_by_date)
-            };
-
-            s3.upload(params, function(err, data) {
-                if (err) {
-                    throw err;
-                }
-                console.log('zoom_meetings.json uploaded to S3');
-            });
-        });
+      console.log(meetingsByDate);
     });
+  });
 };
