0b72dfff019053fa2d1020c868bbe9d88b95a238
Josh King <skierx@gmail.com>
2022-10-23 12:42:42 -0400

    Updating content of cards

--- app.js
+++ app.js
@@ -20,105 +20,174 @@ const SLACK_INVITE_CHANNEL = process.env.SLACK_INVITE_CHANNEL;
 const SLACK_TOPIC_ARN      = process.env.SLACK_TOPIC_ARN;
 const AWS_BUCKET           = process.env.AWS_BUCKET;
 
-const CARDS = {
-  website: {
-    alt:      'Website Logo',
-    bg:       '#f00',
-    icon:     'https://pbs.twimg.com/profile_images/891003621822910464/Pdp_UUB__400x400.jpg',
-    subtitle: 'bostondsa.org',
-    title:    'Website',
-    url:      'https://bostondsa.org/',
-  },
-  wiki: {
-    alt:      'Wiki Logo',
-    bg:       '#fff',
-    icon:     'https://wiki.bostondsa.org/w/resources/assets/rose.jpg',
-    subtitle: 'Chapter knowledge base',
-    title:    'Wiki',
-    url:      'https://wiki.bostondsa.org/',
-  },
-  calendar: {
-    alt:      'Calendar Logo',
-    bg:       '#821d1d',
-    icon:     'https://upload.wikimedia.org/wikipedia/commons/5/56/Apple_Calendar_Icon.png',
-    subtitle: 'Chapter Events',
-    title:    'Calendar',
-    url:      'https://www.bostondsa.org/events/',
-  },
-  slack: {
-    alt:      'Slack Logo',
-    bg:       '#ff4f4f',
-    icon:     'https://upload.wikimedia.org/wikipedia/commons/7/76/Slack_Icon.png',
-    subtitle: 'Join the conversation',
-    title:    'Slack',
-    url:      '/home/slack',
-  },
-  twitter: {
-    alt:      'Twitter Logo',
-    bg:       '#775b7b',
-    icon:     'https://upload.wikimedia.org/wikipedia/it/archive/0/09/20160903181541!Twitter_bird_logo.png',
-    subtitle: 'Follow us on Twitter',
-    title:    'Twitter',
-    url:      'https://twitter.com/Boston_DSA',
-  },
-  blog: {
-    alt:      'Blog Logo',
-    bg:       '#f8b195',
-    icon:     'https://i2.wp.com/bostonpewg.org/wp-content/uploads/2018/03/cropped-pewglogo1.png',
-    style:    'height: 91px;',
-    subtitle: 'Political Education Blog',
-    title:    'Blog',
-    url:      'https://bostonpewg.org/',
-  },
-  vote: {
-    alt:      'Vote',
-    bg:       '#ea290b',
-    icon:     'https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Ballot_box_icon_color.svg/1006px-Ballot_box_icon_color.svg.png',
-    style:    'height: 91px;',
-    subtitle: 'Chapter Voting Discussion',
-    title:    'Voting Discussion',
-    url:      'https://vote.bostondsa.org/'
-  },
-  zoom: {
-    alt:      'Zoom Logo',
-    bg:       '#d0dff7',
-    icon:     'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/Zoom_Communications_Logo.svg/320px-Zoom_Communications_Logo.svg.png',
-    style:    'height: 30px;',
-    subtitle: 'Virtual Meetings',
-    title:    'Zoom',
-    url:      '/home/zoom',
-  },
-  facebook: {
-    alt:      'Facebook Logo',
-    bg:       '#40516e',
-    icon:     'https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/240px-Facebook_f_logo_%282019%29.svg.png',
-    subtitle: 'Facebook',
-    title:    'Facebook',
-    url:      'https://www.facebook.com/BostonDSA',
-  },
-  instagram: {
-    alt:      'Instagram Logo',
-    bg:       '#e9c2ed',
-    icon:     'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/132px-Instagram_logo_2016.svg.png',
-    subtitle: 'Instagram',
-    title:    'Instagram',
-    url:      'https://www.instagram.com/boston_dsa/',
-  },
-  handbook: {
-    alt:      'New Member Handbook',
-    bg:       '#eeeeee',
-    icon:     'https://lh3.googleusercontent.com/vK2eHGgFULWa4G5Nwo6x-_GAgFpjnDPC0ELuGQd6GHLtoKHWXvLK-f1RYuVPFEDHRkGZGnN7clE1l6g4ZNCOeYPXkiZ2NV-_1QrIwdJXXLtVHrrYNDLY69wNIvj28HngNw=w1280',
-    subtitle: 'New Member Handbook',
-    title:    'Information for new members!',
-    url:      'https://sites.google.com/view/boston-dsa-new-member-handbook/home',
-  },
-};
 const ROWS = [
-  Object.values(CARDS).slice(0, 3),
-  Object.values(CARDS).slice(3, 6),
-  Object.values(CARDS).slice(6, 9),
-  Object.values(CARDS).slice(9, 12),
-];
+  {
+    header: 'Member Resources',
+    cards: [
+      [
+        {
+          alt:      'Calendar Logo',
+          bg:       '#821d1d',
+          icon:     'https://upload.wikimedia.org/wikipedia/commons/5/56/Apple_Calendar_Icon.png',
+          subtitle: 'Chapter Events',
+          title:    'Calendar',
+          url:      'https://www.bostondsa.org/events/',
+        },
+        {
+          alt:      'Wiki Logo',
+          bg:       '#fff',
+          icon:     'https://wiki.bostondsa.org/w/resources/assets/rose.jpg',
+          subtitle: 'Chapter knowledge base',
+          title:    'Wiki',
+          url:      'https://wiki.bostondsa.org/',
+        },
+        {
+          alt:      'Slack Logo',
+          bg:       '#ff4f4f',
+          icon:     'https://upload.wikimedia.org/wikipedia/commons/7/76/Slack_Icon.png',
+          subtitle: 'Join the conversation',
+          title:    'Slack',
+          url:      '/home/slack',
+        }
+      ],
+      [
+        {
+          alt:      'Zoom Logo',
+          bg:       '#d0dff7',
+          icon:     'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/Zoom_Communications_Logo.svg/320px-Zoom_Communications_Logo.svg.png',
+          style:    'height: 30px;',
+          subtitle: 'Virtual Meetings',
+          title:    'Zoom',
+          url:      '/home/zoom',
+        },
+        {
+          alt:      'Vote',
+          bg:       '#ea290b',
+          icon:     'https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Ballot_box_icon_color.svg/1006px-Ballot_box_icon_color.svg.png',
+          style:    'height: 91px;',
+          subtitle: 'Chapter Voting Discussion',
+          title:    'Voting Discussion',
+          url:      'https://vote.bostondsa.org/'
+        }
+      ]
+    ]
+  },
+  {
+    header: 'How To Do Things',
+    cards: [
+      [
+        {
+          alt:      'Request an endorsement',
+          bg:       '#eeeeee',
+          icon:     'https://upload.wikimedia.org/wikipedia/commons/a/a1/Google_docs.png',
+          subtitle: 'Request an endorsement',
+          title:    'Endorsements',
+          url:      'https://forms.gle/5cXw9gjy4MFuEDU79',
+        },
+        {
+          alt:      'Request funding',
+          bg:       '#eeeeee',
+          icon:     'https://upload.wikimedia.org/wikipedia/commons/a/a1/Google_docs.png',
+          subtitle: 'Request funding',
+          title:    'Funding',
+          url:      'https://forms.gle/5cXw9gjy4MFuEDU79',
+        },
+        {
+          alt:      'Request a chapter-wide email',
+          bg:       '#eeeeee',
+          icon:     'https://upload.wikimedia.org/wikipedia/commons/a/a1/Google_docs.png',
+          subtitle: 'Request a chapter-wide email',
+          title:    'Bulk email',
+          url:      'https://forms.gle/5cXw9gjy4MFuEDU79',
+        }
+      ],
+      [
+        {
+          alt:      'Request a textbank',
+          bg:       '#eeeeee',
+          icon:     'https://upload.wikimedia.org/wikipedia/commons/a/a1/Google_docs.png',
+          subtitle: 'Request a textbank',
+          title:    'Textbank',
+          url:      'https://forms.gle/ijrzYeVTwr4qHWEd7',
+        }
+      ]
+    ]
+  },
+  {
+    header: 'New Member Guides',
+    cards: [
+      [
+        {
+          alt:      'New Member Handbook',
+          bg:       '#eeeeee',
+          icon:     'https://upload.wikimedia.org/wikipedia/commons/thumb/2/25/Book_icon_%28closed%29_-_Red_and_gold.svg/137px-Book_icon_%28closed%29_-_Red_and_gold.svg.png',
+          subtitle: 'New Member Handbook',
+          title:    'New Member Handbook',
+          url:      'https://sites.google.com/view/boston-dsa-new-member-handbook/home',
+        },
+        {
+          alt:      'Chapter Orientations',
+          bg:       '#eeeeee',
+          icon:     'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/Compass_icon_matte.svg/240px-Compass_icon_matte.svg.png',
+          subtitle: 'Chapter Orientations',
+          title:    'Orientation',
+          url:      'https://actionnetwork.org/campaigns/boston-dsa-member-orientations',
+        }
+      ]
+    ]
+  },
+  {
+    header: 'Find us',
+    cards: [
+      [
+        {
+          alt:      'Website Logo',
+          bg:       '#f00',
+          icon:     'https://pbs.twimg.com/profile_images/891003621822910464/Pdp_UUB__400x400.jpg',
+          subtitle: 'bostondsa.org',
+          title:    'Website',
+          url:      'https://bostondsa.org/',
+        },
+        {
+          alt:      'Facebook Logo',
+          bg:       '#40516e',
+          icon:     'https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/240px-Facebook_f_logo_%282019%29.svg.png',
+          subtitle: 'Facebook',
+          title:    'Facebook',
+          url:      'https://www.facebook.com/BostonDSA',
+        },
+        {
+          alt:      'Instagram Logo',
+          bg:       '#e9c2ed',
+          icon:     'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/132px-Instagram_logo_2016.svg.png',
+          subtitle: 'Instagram',
+          title:    'Instagram',
+          url:      'https://www.instagram.com/boston_dsa/',
+        }
+      ],
+      [
+        {
+          alt:      'Twitter Logo',
+          bg:       '#775b7b',
+          icon:     'https://upload.wikimedia.org/wikipedia/it/archive/0/09/20160903181541!Twitter_bird_logo.png',
+          subtitle: 'Follow us on Twitter',
+          title:    'Twitter',
+          url:      'https://twitter.com/Boston_DSA',
+        },
+        {
+          alt:      'Blog Logo',
+          bg:       '#f8b195',
+          icon:     'https://i2.wp.com/bostonpewg.org/wp-content/uploads/2018/03/cropped-pewglogo1.png',
+          style:    'height: 91px;',
+          subtitle: 'Political Education Blog',
+          title:    'Blog',
+          url:      'https://bostonpewg.org/',
+        }
+      ]
+    ]
+  },
+
+]
 
 const keycloakConfig = {
   clientId: process.env.KEYCLOAK_CLIENT_ID,
